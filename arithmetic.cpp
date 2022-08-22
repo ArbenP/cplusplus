f3ca3a55f7dae736a82e26ea6d75deaa6733e639
Arben P <arbengamer@gmail.com>
2022-08-22 23:59:36 +0100

    save

---
+++ arithmetic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+
+  int num1 = 10, num2 = 20;
+
+  std::cout << num1 << " + " << num2 << " = "<< num1+num2 << '\n';
+  std::cout << num1 << " - " << num2 << " = "<< num1-num2 << '\n';
+
+  std::cout << num1 << " * " << num2 << " = "<< num1*num2 << '\n';
+  std::cout << num1 << " / " << num2 << " = "<< num1/num2 << '\n';
+
+
+    return 0;
+}
---
+++ userinput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// cout << (insertion operator)
+// cin >> (extraction operator)
+
+int main()
+{
+    std::string name;
+
+    std::cout << "What is your name?: "
+    std::cin >>
+
+
+    return 0;
+}
